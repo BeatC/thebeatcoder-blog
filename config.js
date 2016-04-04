7b8d6a4d4b0aac6e20e9abc7cf53911e5bb61afc
Illia Daynatovich <idayna@MACMINI258.local>
2016-04-04 16:37:32 +0300

    edit config

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'thebeatcoder-blog.herokuapp.com',
+        url: 'http://localhost',
         mail: {},
         database: {
             client: 'sqlite3',
